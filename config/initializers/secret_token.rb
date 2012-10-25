6f1e5dc631c8fcc5999550b7af0bc0339dcbdc20
Alexey Mirniy <alexey.mirniy@gmail.com>
2012-10-25 14:34:19 +0300

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DpRails::Application.config.secret_token = '32dc8eb2100cbaa0b56bf5f0019b1e6e728f4cff71d152d1d59f190325bd3d9b2a6af95c4cc0dc6145546f51d6a96c14d067aa298f8f56a19ff9e5ceec80f295'
